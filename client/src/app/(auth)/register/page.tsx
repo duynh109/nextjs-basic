db1a680e0b39efbd7655b064297d19ad183146a3
duynh109 <duy10092002@gmail.com>
2024-08-18 18:23:54 +0700

    code trang register

---
+++ client/src/app/(auth)/register/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import RegisterForm from '@/app/(auth)/register/resgister-form';
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1 className="text-xl font-semibold text-center">Đăng ký</h1>
+      <div className="flex justify-center">
+        <RegisterForm />
+      </div>
+    </div>
+  );
+}
