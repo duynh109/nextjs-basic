8cbd82fce958eda74052ee073dd201152871ff61
duynh109 <duy10092002@gmail.com>
2024-08-27 21:09:32 +0700

    code trang login

--- client/src/app/(auth)/login/login-form.tsx
+++ client/src/app/(auth)/login/login-form.tsx
@@ -5,48 +5,65 @@ import { useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { RegisterBody, RegisterBodyType } from '@/schemaValidations/auth.schema';
+import { LoginBody, LoginBodyType } from '@/schemaValidations/auth.schema';
 import envConfig from '@/config';
+import { useToast } from '@/components/ui/use-toast';
 
-export default function RegisterForm() {
-  const form = useForm<RegisterBodyType>({
-    resolver: zodResolver(RegisterBody),
+export default function LoginForm() {
+  const { toast } = useToast();
+  const form = useForm<LoginBodyType>({
+    resolver: zodResolver(LoginBody),
     defaultValues: {
       email: '',
-      name: '',
       password: '',
-      confirmPassword: '',
     },
   });
 
   // 2. Define a submit handler.
-  async function onSubmit(values: RegisterBodyType) {
-    const result = await fetch(`${envConfig.NEXT_PUBLIC_API_ENDPOINT}/auth/register`, {
-      body: JSON.stringify(values),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-    }).then((res) => res.json());
-    console.log(result);
+  async function onSubmit(values: LoginBodyType) {
+    try {
+      const result = await fetch(`${envConfig.NEXT_PUBLIC_API_ENDPOINT}/auth/login`, {
+        body: JSON.stringify(values),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      }).then(async (res) => {
+        const payload = await res.json();
+        const data = {
+          status: res.status,
+          payload,
+        };
+        if (!res.ok) {
+          throw data;
+        }
+        return data;
+      });
+      toast({
+        description: result.payload.message,
+      });
+    } catch (error: any) {
+      const errors = error.payload.errors as { field: string; message: string }[];
+      const status = error.status as number;
+      if (status === 422) {
+        errors.forEach((error) => {
+          form.setError(error.field as 'email' | 'password', {
+            type: 'sever',
+            message: error.message,
+          });
+        });
+      } else {
+        toast({
+          title: 'Lỗi',
+          description: error.payload.message,
+          variant: 'destructive',
+        });
+      }
+    }
   }
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 max-w-[600px] w-full" noValidate>
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Tên</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
         <FormField
           control={form.control}
           name="email"
@@ -75,22 +92,9 @@ export default function RegisterForm() {
             </FormItem>
           )}
         />
-        <FormField
-          control={form.control}
-          name="confirmPassword"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Nhập lại mật khẩu</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" type="password" {...field} />
-              </FormControl>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
         <Button className="!mt-8 w-full" type="submit">
-          Đăng ký
+          Đăng nhập
         </Button>
       </form>
     </Form>
